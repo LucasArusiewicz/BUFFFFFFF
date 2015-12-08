b7fca9608f0b365a59f391f930f3fdf1b85acd3d
LucasArusiewicz <lucasarusiewicz@gmail.com>
2015-12-08 19:54:29 -0200

    1

--- app/src/main/java/gocode/lucas/felipe/alisson/buff/ResultadoActivity.java
+++ app/src/main/java/gocode/lucas/felipe/alisson/buff/ResultadoActivity.java
@@ -36,7 +36,7 @@ public class ResultadoActivity extends AppCompatActivity {
             tvResultado.setText("Você Ganhou !");
         } else {
             if (errou){
-                new CountDownTimer(3000,1000){
+                new CountDownTimer(5000,1000){
 
                     @Override
                     public void onTick(long millisUntilFinished) {
